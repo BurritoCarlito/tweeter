b271dc628d9980d5c395e33b65ffffccb057f714
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-05-26 02:28:55 +0000

    created validation, alert messages if not valid tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,59 +5,27 @@
  */
 
 $(document).ready(() => {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
-
 
     // const oneTweet = createTweetElement(data)
     // $('.feed').append(oneTweet)
 
-    function createTweetElement(tweetData) {
-      const tweet = `<article class="tweets">
-            <header>
-              <div id="avatar">
-                <div class="avatar-Img">
-                <img src=${tweetData.user.avatars}></img>
-                <h2>
-                  ${tweetData.user.name}
-                </h2>
-                </div>
-                <div class="username">
-                ${tweetData.user.handle}
-                </div>
+  function createTweetElement(tweetData) {
+    const tweet = `<article class="tweets">
+      <header>
+        <div id="avatar">
+          <div class="avatar-Img">
+            <img src=${tweetData.user.avatars}></img>
+              <h2>${tweetData.user.name}</h2>
+              </div>
+              <div class="username">${tweetData.user.handle}</div>
               </div>
               </header>
               <div class="tweet">
-                <p>
-                  ${tweetData.content.text}
-                </p>
+                <p>${tweetData.content.text}</p>
               </div>
               <footer>
                 <div class="timeStamp">
-                    ${tweetData.created_at}
+                    ${timeago.format(tweetData.created_at)}
                 </div>
                 <div class="icons">
                   <i class="fa-solid fa-flag"></i>
@@ -80,26 +48,37 @@ $(document).ready(() => {
     return;
   };
   
-  renderTweets(data);
-
   $(".form").submit(function(event) {
     console.log("CLICK");
     event.preventDefault()
 
+    //create helper function to better access the array value, given the html input name
+
+    let info = $(".form").serializeArray();
+      if (info[0].value.length === 0 ) {
+        alert("Tweet Cannot be Empty!");
+      } else if (info[0].value.length > 140) {
+        alert("Tweet Cannot be longer than 140 characters")
+      } else {
+  
     $.ajax({
       type: "POST",
       url: "/tweets",
       data: $(".form").serialize(),
       success: () => {
-        console.log("we did it")
+        console.log($(".form").serialize());
       } 
     });
-
-
-    // let post = $.post("http://localhost:8080/");
-    // console.log(post);
+  }
   });
 
-  
-})
+  function loadTweets() {
+    console.log("loading tweets")
+    $.ajax("/tweets", { method: "GET" })
+      .then( tweets => renderTweets(tweets))
+  };
+
+  loadTweets();
+
+});
 
